6f96c2ae291182b8946edb19a6c1bcfd8e0299c8
LarissaOkamoto <larissa.okamoto@sptech.school>
2025-12-04 20:34:41 -0300

    teste vm v4

--- web-data-viz/src/models/usuarioModel.js
+++ web-data-viz/src/models/usuarioModel.js
@@ -27,7 +27,7 @@ function cadastrarUsuario(nome, email, senha, fkEmpresa, fkUsuarioResponsavel) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkEmpresa);
     
     var instrucaoSql = `
-        INSERT INTO Usuario (nome, email, senha, fkEmpresa, fkResponsavel) VALUES ('${nome}', '${email}', '${senha}', '${fkEmpresa}', '${fkResponsavel}' );
+        INSERT INTO Usuario (nome, email, senha, fkEmpresa, fkUsuarioResponsavel) VALUES ('${nome}', '${email}', '${senha}', '${fkEmpresa}', '${fkUsuarioResponsavel}' );
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
